bba393d24785f10fd906a2238419b0edb1da05e6
tpxxy1311 <65901921+tpxxy1311@users.noreply.github.com>
2024-06-17 15:49:21 +0200

    git hub linter plugin test

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ buildscript {
 
  */
 plugins {
+    kotlin("multiplatform") version "1.5.31"
+    id("org.jetbrains.compose") version "1.6.0"
     kotlin("jvm") version "1.5.31" // Stelle sicher, dass dies kompatibel ist
     kotlin("plugin.serialization") version "1.5.31" // Stelle sicher, dass dies kompatibel ist
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
